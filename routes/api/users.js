de56865f34ee7d1ac8a48c75b452ef3d46516e23
Roshan <roshanlakra19122000@gmail.com>
2023-03-26 23:45:06 +0530

    validator function

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,27 @@
-const express =require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // router GET api/users
 
-router.get('/',(req,res)=>res.send('user route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enter a password with 6 or more character'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
-module.exports=router;
+module.exports = router;
