5e91f05038e523ab2fcf17a61a3e49d6fa2d8fae
Roshan <roshanlakra19122000@gmail.com>
2023-06-05 09:19:58 +0530

    for deployment

--- client/src/component/test/ChildComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ChildComponent(props) {
-  return <div></div>;
-}
-
-export default ChildComponent;
--- server.js
+++ server.js
@@ -9,20 +9,20 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-// static files
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
-
-// app.get('/',(req,res)=>res.send('API RUNNING!'));
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000;
+// app.get('/',(req,res)=>res.send('API RUNNING!'));
+
+//static files
+
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+const PORT = process.env.Port || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
